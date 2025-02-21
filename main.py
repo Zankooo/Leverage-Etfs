a9436f93691d2b3a52bd8d6c7c40e57667078c7b
Zankooo <stankovic.zan@gmail.com>
2025-02-21 12:41:05 +0100

    prvi commit

---
+++ main.py
@@ -0,0 +1,50 @@
+initial_investment = int(input("Vpisi zacetno investicijo (eur): "))
+interest_rate = float(input("Vpisi donosnost v %: "))
+time_period = int(input("Vpisi časovni period (v letih): "))
+
+"""
+FUNKCIJA; OBRESTNO OBRESTOVANJE
+@:param principal: zacetna investicija
+@:param rate: donosnost v %
+@:param time: čas v letih
+@:return: koliko imaš skupaj
+"""
+def calculate_compound_interest(principal, rate, time):
+    print("----------------------------------------------------------------")
+    print("COMPOUND OBRESTOVANJE")
+    interest_rate_decimal = rate / 100  # Uporabnik vnese npr. 10%, mi ga spremenimo v 0.1
+    compounded_cash = principal
+    for i in range(0, time):
+        compounded_cash = compounded_cash * (1 + interest_rate_decimal)
+    profit = compounded_cash - principal  # Čisti dobiček
+    print(f"V vseh teh letih smo zaslužili čistega {profit:.2f} eur")
+    print(f"Vse skupaj pa je sedaj keša: {compounded_cash:.2f} eur")
+    return profit
+
+"""
+FUNKCIJA; NAVADNO OBRESTOVANJE
+@:param principal: zacetna investicija
+@:param rate: donosnost v %
+@:param time: čas v letih
+@:return: koliko imaš skupaj
+"""
+def calculate_simple_interest(principal, rate, time):
+    obresti = principal * rate * time / 100
+    skupaj = principal + obresti
+    print("----------------------------------------------------------------")
+    print("NAVADNO OBRESTOVANJE")
+    print(f"V vseh teh letih smo zaslužili čistega: {obresti} eur")
+    print(f"Vse skupaj pa je sedaj keša: {skupaj} eur")
+    return skupaj
+
+
+calculate_simple_interest(initial_investment,interest_rate,time_period)
+calculate_compound_interest(initial_investment, interest_rate, time_period)
+
+
+
+
+# morm narest to verzijo od leta pač kokr mam podatke do recimo 2024 da
+# radotu pokazm kaj mislm pac
+# za vsa leta ampak da menjam kolko pade sp500 k kupis leverage in vplacila in tako
+#ampak leta pa ostanejo ista
