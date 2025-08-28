f50d3a889207b020ff4774db444cdee2c11ed957
Zankooo <stankovic.zan@gmail.com>
2025-08-28 20:25:24 +0200

    updejt

---
+++ main-3.py
@@ -0,0 +1,9 @@
+# ti dolocis v katerega prvo investiras
+# dolocis koliko mora biti dol osnvoen da kupis 2x ali 3x
+# dolocis interval
+# in ti spet on izracuna za vsa leta oz intervale
+
+# to bo funkcija ki bo izracunala vse skupaj
+# 15 let recimo. ce je osnoven 5% dol kupimo tistega v mesecu 2x, ce 10% kupimo 3x
+# to bomo probali kakko bo ker je fact da je fajn kupovat ko je dol cim vecji leverage
+
